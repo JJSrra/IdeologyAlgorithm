e77d963e349741b52834dc6562767e181fd040ec
Juanjo Sierra <jjsierra103@gmail.com>
2018-08-19 20:39:58 +0200

    Modified IA algorithm to better fit with workflow methodology in the research

--- IA.py
+++ IA.py
@@ -1,4 +1,3 @@
-import optproblems.cec2005
 import numpy as np
 from Politician import Politician
 from Party import Party
@@ -7,14 +6,11 @@ from constrained_kmeans import constrained_kmeans
 import copy
 import sys
 
-
 class IdeologyAlgorithm:
     def __init__(self, n_parties, politicians, R, function, function_index, max_evaluations, desertion_threshold):
         if (politicians % n_parties != 0):
             print("It's impossible to create", n_parties, "parties with the same amount of politicians in each one.")
             raise SystemExit
-
-        np.random.seed(3)
 
         self.__n_parties = n_parties
         self.__politicians = politicians
@@ -24,8 +20,6 @@ class IdeologyAlgorithm:
         self.__evaluations = 0
         self.__max_evaluations = max_evaluations
         self.__desertion_threshold = desertion_threshold
-
-        self.__best_value_file = open("./results/best_value_" + str(function_index) + "_dim" + str(self.__function.num_variables) + ".dat", "w")
 
         self.__best_solution = Politician(self.__function, self.__function_index)
         # The maximum available value
@@ -224,7 +218,6 @@ class IdeologyAlgorithm:
             # Update the leader of each party
             leaders = np.array([party.get_leader() for party in self.__population])
             global_leader = copy.deepcopy(sorted(leaders, key=lambda p: p.get_fitness())[0])
-            self.__best_value_file.write("{} {} \n".format(self.__evaluations, global_leader.get_fitness()))
 
             # We must store the best solution in every iteration
             if (global_leader.get_fitness() < self.__best_solution.get_fitness()):
@@ -253,119 +246,12 @@ class IdeologyAlgorithm:
             [self.__update_party(party) for party in self.__population]
             # We sort the population to get the best individuals first
             self.__sort_population()
-
-        self.__best_value_file.close()
         
         leaders = np.array([party.get_leader() for party in self.__population])
         if sorted(leaders, key=lambda p: p.get_fitness())[0].get_fitness() < self.__best_solution.get_fitness():
-            return sorted(leaders, key=lambda p: p.get_fitness())[0]
+            return sorted(leaders, key=lambda p: p.get_fitness())[0].get_fitness()
         else:
-            return self.__best_solution
+            return self.__best_solution.get_fitness()
 
     def get_population(self):
         return self.__population
-
-
-def main():
-    dimension = 30
-    n_parties = 5
-    politicians = 150
-    R = 0.05
-    max_evaluations = 10000*dimension
-    desertion_threshold = 10
-
-    f = optproblems.cec2005.F1(dimension)
-    algorithm = IdeologyAlgorithm(n_parties=n_parties, politicians=politicians, R=R, function=f, function_index=1,
-                                  max_evaluations=max_evaluations, desertion_threshold=desertion_threshold)
-    best = algorithm.ideology_algorithm()
-    print("F1 {} {} {}".format(best.get_fitness(), f.bias,
-                                     np.round(np.absolute((best.get_fitness() - f.bias) / f.bias * 100), 4)))
-    '''
-    f = optproblems.cec2005.F2(dimension)
-    algorithm = IdeologyAlgorithm(n_parties=n_parties, politicians=politicians, R=R, function=f, function_index=2,
-                                  max_evaluations=max_evaluations, desertion_threshold=desertion_threshold)
-    best = algorithm.ideology_algorithm()
-    print("F2 {} {} {}".format(best.get_fitness(), f.bias,
-                                     np.round(np.absolute((best.get_fitness() - f.bias) / f.bias * 100), 4)))
-
-    f = optproblems.cec2005.F5(dimension)
-    algorithm = IdeologyAlgorithm(n_parties=n_parties, politicians=politicians, R=R, function=f, function_index=5,
-                                  max_evaluations=max_evaluations, desertion_threshold=desertion_threshold)
-    best = algorithm.ideology_algorithm()
-    print("F5 {} {} {}".format(best.get_fitness(), f.bias,
-                                     np.round(np.absolute((best.get_fitness() - f.bias) / f.bias * 100), 4)))
-
-    f = optproblems.cec2005.F6(dimension)
-    algorithm = IdeologyAlgorithm(n_parties=n_parties, politicians=politicians, R=R, function=f, function_index=6,
-                                  max_evaluations=max_evaluations, desertion_threshold=desertion_threshold)
-    best = algorithm.ideology_algorithm()
-    print("F6 {} {} {}".format(best.get_fitness(), f.bias,
-                                     np.round(np.absolute((best.get_fitness() - f.bias) / f.bias * 100), 4)))
-
-    f = optproblems.cec2005.F8(dimension)
-    algorithm = IdeologyAlgorithm(n_parties=n_parties, politicians=politicians, R=R, function=f, function_index=8,
-                                  max_evaluations=max_evaluations, desertion_threshold=desertion_threshold)
-    best = algorithm.ideology_algorithm()
-    print("F8 {} {} {}".format(best.get_fitness(), f.bias,
-                                     np.round(np.absolute((best.get_fitness() - f.bias) / f.bias * 100), 4)))
-
-    f = optproblems.cec2005.F9(dimension)
-    algorithm = IdeologyAlgorithm(n_parties=n_parties, politicians=politicians, R=R, function=f, function_index=9,
-                                  max_evaluations=max_evaluations, desertion_threshold=desertion_threshold)
-    best = algorithm.ideology_algorithm()
-    print("F9 {} {} {}".format(best.get_fitness(), f.bias,
-                                     np.round(np.absolute((best.get_fitness() - f.bias) / f.bias * 100), 4)))
-
-    f = optproblems.cec2005.F10(dimension)
-    algorithm = IdeologyAlgorithm(n_parties=n_parties, politicians=politicians, R=R, function=f, function_index=10,
-                                  max_evaluations=max_evaluations, desertion_threshold=desertion_threshold)
-    best = algorithm.ideology_algorithm()
-    print("F10 {} {} {}".format(best.get_fitness(), f.bias,
-                                      np.round(np.absolute((best.get_fitness() - f.bias) / f.bias * 100), 4)))
-
-    f = optproblems.cec2005.F11(dimension)
-    algorithm = IdeologyAlgorithm(n_parties=n_parties, politicians=politicians, R=R, function=f, function_index=11,
-                                  max_evaluations=max_evaluations, desertion_threshold=desertion_threshold)
-    best = algorithm.ideology_algorithm()
-    print("F11 {} {} {}".format(best.get_fitness(), f.bias,
-                                      np.round(np.absolute((best.get_fitness() - f.bias) / f.bias * 100), 4)))
-
-    f = optproblems.cec2005.F13(dimension)
-    algorithm = IdeologyAlgorithm(n_parties=n_parties, politicians=politicians, R=R, function=f, function_index=13,
-                                  max_evaluations=max_evaluations, desertion_threshold=desertion_threshold)
-    best = algorithm.ideology_algorithm()
-    print("F13 {} {} {}".format(best.get_fitness(), f.bias,
-                                      np.round(np.absolute((best.get_fitness() - f.bias) / f.bias * 100), 4)))
-
-    f = optproblems.cec2005.F14(dimension)
-    algorithm = IdeologyAlgorithm(n_parties=n_parties, politicians=politicians, R=R, function=f, function_index=14,
-                                  max_evaluations=max_evaluations, desertion_threshold=desertion_threshold)
-    best = algorithm.ideology_algorithm()
-    print("F14 {} {} {}".format(best.get_fitness(), f.bias,
-                                      np.round(np.absolute((best.get_fitness() - f.bias) / f.bias * 100), 4)))
-
-    f = optproblems.cec2005.F17(dimension)
-    algorithm = IdeologyAlgorithm(n_parties=n_parties, politicians=politicians, R=R, function=f, function_index=17,
-                                  max_evaluations=max_evaluations, desertion_threshold=desertion_threshold)
-    best = algorithm.ideology_algorithm()
-    print("F17 {} {} {}".format(best.get_fitness(), f.bias,
-                                      np.round(np.absolute((best.get_fitness() - f.bias) / f.bias * 100), 4)))
-
-    f = optproblems.cec2005.F22(dimension)
-    algorithm = IdeologyAlgorithm(n_parties=n_parties, politicians=politicians, R=R, function=f, function_index=22,
-                                  max_evaluations=max_evaluations, desertion_threshold=desertion_threshold)
-    best = algorithm.ideology_algorithm()
-    print("F22 {} {} {}".format(best.get_fitness(), f.bias,
-                                      np.round(np.absolute((best.get_fitness() - f.bias) / f.bias * 100), 4)))
-
-    f = optproblems.cec2005.F24(dimension)
-    algorithm = IdeologyAlgorithm(n_parties=n_parties, politicians=politicians, R=R, function=f, function_index=24,
-                                  max_evaluations=max_evaluations, desertion_threshold=desertion_threshold)
-    best = algorithm.ideology_algorithm()
-    print("F24 {} {} {}".format(best.get_fitness(), f.bias,
-                                      np.round(np.absolute((best.get_fitness() - f.bias) / f.bias * 100), 4)))
-    '''
-
-
-if __name__ == "__main__":
-    main()
