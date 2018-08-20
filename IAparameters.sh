79d6fa8159a60f53c2e09033735c16dfd20c2f4a
Juanjo Sierra <jjsierra103@gmail.com>
2018-08-20 20:50:49 +0200

    Changed output files and job name for better visualization in SLURM server

--- IAparameters.sh
+++ IAparameters.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 #
 # The name of the job, can be whatever makes sense to you
-#SBATCH -J param-est-IA
+#SBATCH -J IA-param
 
 # The job should be placed into the queue 'larga'.
 #SBATCH -p larga
 
 # Redirect output stream to this file.
-#SBATCH -o param-est-IA.dat
+#SBATCH -o IA-param.dat
 
 # Redirect error stream to this file.
-#SBATCH -e param-est-IA-err.dat
+#SBATCH -e IA-param-err.dat
 
 #_______________________________________________________________________________
 
